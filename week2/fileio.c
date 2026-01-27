0f5ecbb292b284ae695345f7e692cb9be6e685fa
MeiLi Charles <gymmyp1@gmail.com>
2026-01-27 16:42:02 -0500

    add better comments to fileio.c

--- week2/fileio.c
+++ week2/fileio.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	FILE *in;
+	FILE *in; //declare file variable
 	char name[50];
 
-	in = fopen("names.dat", "r");
+	in = fopen("names.dat", "r"); //open file in read mode
 
-	//read from file as long as we haven't hit eof
+	//read from file as long as one name was read
 	//fscanf stops at whitespace
-	while(fscanf(in, "%s", name) != EOF) {
+	while(fscanf(in, "%s", name)  == 1) {
 		printf("Name read: %s\n", name);
-		//fflush(stdout); //make sure the printf statment on prev line prints
+		//fflush(stdout); //make sure the printf statment on previous line prints
 	}
 
-	//ALTERATIVE: read from file as long as we haven't hit eof
-	// this reads whole lines, including spaces.
+	//ALTERNATIVE: read from file until fgets returns NULL, which is what happens when EOF is reached
+	// fgets reads until \n.
 	/*while(fgets(name, 50, in)) {
 		printf("Name read: %s\n", name);
 	}*/
